df72998c3957bdea745b9c0fc925ac2114a545ce
Andrew Sutherland <asutherland@asutherland.org>
2021-06-16 19:24:23 +0000

    Apply the Gaia Apache2 License on a per-file basis to make the license linter happy.
    The license linter was upset that the files did not each have a license.
    
    GELAM was developed as part of the Boot2Gecko project under the Apache 2 license,
    as can be found at https://github.com/mozilla-b2g/gaia/blob/master/LICENSE
    
    Depends on D118059
    
    Differential Revision: https://phabricator.services.mozilla.com/D118060

--- browser/components/companion/services/workshop/src/backend/engine_glue.js
+++ browser/components/companion/services/workshop/src/backend/engine_glue.js
@@ -1,4 +1,20 @@
 /**
+ * Copyright 2021 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * The home for all engine abstractions; if you add a new account type or engine
  * then this is ideally the only place it gets added.  Some changes may happen
  * in the future to define these all as optional extensions that the app_logic
--- browser/components/companion/services/workshop/src/backend/search/match_regexp_or_string.js
+++ browser/components/companion/services/workshop/src/backend/search/match_regexp_or_string.js
@@ -1,4 +1,20 @@
 /**
+ * Copyright 2021 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Check if a string or a regexp matches an input and if it does, it returns a
  * 'return value' as RegExp.exec does.  Note that the 'index' of the returned
  * value will be relative to the provided `fromIndex` as if the string had been
