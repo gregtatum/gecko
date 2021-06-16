572908b94eea378ed926ac07d92013a454e9bfb3
Andrew Sutherland <asutherland@asutherland.org>
2021-06-16 19:24:23 +0000

    Make companion workshop esbuild script pass eslint check.
    Differential Revision: https://phabricator.services.mozilla.com/D118059

--- browser/components/companion/services/workshop/esbuild.js
+++ browser/components/companion/services/workshop/esbuild.js
@@ -1,16 +1,17 @@
-require('esbuild').buildSync({
+/*eslint-env node*/
+require("esbuild").buildSync({
   entryPoints: {
-    'workshop-api-built': './src/main-frame-setup',
-    'workshop-worker-built': './src/backend/worker-setup',
+    "workshop-api-built": "./src/main-frame-setup",
+    "workshop-worker-built": "./src/backend/worker-setup",
   },
-  platform: 'browser',
-  target: 'esnext',
+  platform: "browser",
+  target: "esnext",
   bundle: true,
   write: true,
-  outdir: 'build',
-  resolveExtensions: ['.js'],
-  globalName: 'WorkshopAPI',
+  outdir: "build",
+  resolveExtensions: [".js"],
+  globalName: "WorkshopAPI",
   banner: {
-    js: '// THIS IS A GENERATED FILE, DO NOT EDIT DIRECTLY',
+    js: "// THIS IS A GENERATED FILE, DO NOT EDIT DIRECTLY",
   },
 });
