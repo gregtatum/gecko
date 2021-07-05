466d4fd0256674e5e44a0a13a14c3be5d76499c9
Dave Townsend <dtownsend@oxymoronical.com>
2021-07-05 13:08:49 +0100

    Force nightly-pine branding.
    Differential Revision: https://phabricator.services.mozilla.com/D119094

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -35,7 +35,7 @@ BROWSER_PROCLIENT_URL=chrome://browser/content/browser-proclient.xhtml
 # For mozilla-beta, mozilla-release, or mozilla-central repositories, use
 # "unofficial" branding.
 # For the mozilla-aurora repository, use "aurora".
-MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
+MOZ_BRANDING_DIRECTORY=browser/branding/nightly-pine
 MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
 MOZ_APP_ID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}
 
