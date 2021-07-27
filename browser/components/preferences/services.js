73fd4720b4b872055ba5c4c2e0045a0ad33779d8
Mark Banner <standard8@mozilla.com>
2021-07-27 11:28:12 +0000

    Fix ESLint issue in services.js. r=mtigley
    import-globals-from can struggle with cyclic dependencies (bug 13954260).
    
    Differential Revision: https://phabricator.services.mozilla.com/D120851

--- browser/components/preferences/services.js
+++ browser/components/preferences/services.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* import-globals-from preferences.js */
 /* import-globals-from sync.js */
 
 const { OnlineServices } = ChromeUtils.import(
