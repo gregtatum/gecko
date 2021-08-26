3b93a30317d9b00bc3954b81a156b1bea1a3f63b
Mark Striemer <mstriemer@mozilla.com>
2021-08-26 09:51:43 -0500

    MR2-604 - Fix eslint and license issues

--- browser/components/companion/vendor/lit/index.js
+++ browser/components/companion/vendor/lit/index.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 export * from "lit";
 export { asyncAppend } from "lit/directives/async-append.js";
 export { asyncReplace } from "lit/directives/async-replace.js";
--- browser/components/companion/vendor/lit/rollup.config.js
+++ browser/components/companion/vendor/lit/rollup.config.js
@@ -1,4 +1,8 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "index.js",
