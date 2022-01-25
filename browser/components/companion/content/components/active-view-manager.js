b26ce3d927582df6bd20cf0ee73a04ebc06df11d
Mike Conley <mconley@mozilla.com>
2022-01-25 21:12:47 +0000

    MR2-1344 - Add a Snapshot with the user title when a user title is created. r=prathiksha
    Differential Revision: https://phabricator.services.mozilla.com/D136767

--- browser/components/companion/content/components/active-view-manager.js
+++ browser/components/companion/content/components/active-view-manager.js
@@ -424,7 +424,7 @@ export default class ActiveViewManager extends window.MozHTMLElement {
   }
 
   pageActionEditViewTitle(event) {
-    window.gGlobalHistory.updateUserTitle(
+    window.gGlobalHistory.setUserTitle(
       this.#pageActionView,
       event.target.value
     );
