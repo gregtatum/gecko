113c0b1d72f89cfcc1e4bd2cef54cb01d373f74f
Mike Conley <mconley@mozilla.com>
2022-02-11 20:53:03 +0000

    MR2-1502 - Part 1: Move history carousel browser window controller code into HistoryCarouselParent.jsm. r=cmkm
    Differential Revision: https://phabricator.services.mozilla.com/D138310

--- browser/base/content/browser-pinebuild.js
+++ browser/base/content/browser-pinebuild.js
@@ -4,6 +4,14 @@
 
 /* eslint-env mozilla/browser-window */
 
+XPCOMUtils.defineLazyModuleGetters(this, {
+  HistoryCarousel: "resource:///actors/HistoryCarouselParent.jsm",
+});
+
+XPCOMUtils.defineLazyGetter(this, "gHistoryCarousel", () => {
+  return new HistoryCarousel(window);
+});
+
 var PineBuildUIUtils = {
   init() {
     window.addEventListener("deactivate", this);
@@ -21,7 +29,7 @@ var PineBuildUIUtils = {
   },
 
   delayedStartup() {
-    HistoryCarouselUI.init();
+    window.top.gHistoryCarousel.init();
   },
 
   copy(anchor, string) {
@@ -50,102 +58,3 @@ var PineBuildUIUtils = {
 };
 
 PineBuildUIUtils.init();
-
-/**
- * HistoryCarouselUI is responsible for interpreting whether or not
- * the user has interacted with the back button in a way that indicates
- * that they want to show the history carousel. Currently, there are
- * two interactions that do this:
- *
- * 1. A long-press on the back button
- * 2. Frequent clicks on the back button within a time period. Currently,
- *    that's CLICK_COUNT_THRESHOLD clicks during a
- *    CLICK_COUNT_TIMEOUT_MS period.
- */
-var HistoryCarouselUI = {
-  _timerID: null,
-  _clickCount: 0,
-  _openedByLongPress: false,
-
-  /**
-   * Sets up the event handlers on the back button.
-   */
-  init() {
-    if (
-      !Services.prefs.getBoolPref("browser.pinebuild.megaback.enabled", false)
-    ) {
-      return;
-    }
-
-    let pbBackButton = document.getElementById("pinebuild-back-button");
-
-    // We need to add this capturing click event handler before adding
-    // to the gClickAndHoldListenersOnElement to ensure the click event
-    // handler is called first. That gives us an opportunity to cancel
-    // the back button click command from running if the user long
-    // presses on the back button.
-    pbBackButton.addEventListener("click", this, { capture: true });
-
-    gClickAndHoldListenersOnElement.add(pbBackButton, () => {
-      // The user longpressed, so set this flag to prevent the back
-      // command from being fired on click.
-      this._openedByLongPress = true;
-      gGlobalHistory.showHistoryCarousel(true);
-    });
-
-    XPCOMUtils.defineLazyPreferenceGetter(
-      this,
-      "CLICK_COUNT_TIMEOUT_MS",
-      "browser.pinebuild.megaback.click-count-timeout-ms",
-      3000
-    );
-
-    XPCOMUtils.defineLazyPreferenceGetter(
-      this,
-      "CLICK_COUNT_THRESHOLD",
-      "browser.pinebuild.megaback.click-count-threshold",
-      5
-    );
-  },
-
-  handleEvent(event) {
-    if (event.type != "click") {
-      return;
-    }
-    // This is kind of a hack, but if this flag is set, then we
-    // prevent the click event from continuing to propagate, and
-    // call preventDefault. This prevents both the
-    // gClickAndHoldListenersOnElement command and the default
-    // XUL command machinery from firing. We then clear the flag
-    // to make sure that clicks continue to work.
-    if (this._openedByLongPress) {
-      event.preventDefault();
-      event.stopImmediatePropagation();
-      this._openedByLongPress = false;
-      return;
-    }
-
-    if (!this._timerID) {
-      this._clickCount = 1;
-      this._timerID = setTimeout(() => {
-        this.resetCount();
-      }, this.CLICK_COUNT_TIMEOUT_MS);
-    } else {
-      this._clickCount++;
-      if (this._clickCount >= this.CLICK_COUNT_THRESHOLD) {
-        clearTimeout(this._timerID);
-        this.resetCount();
-        gGlobalHistory.showHistoryCarousel(true);
-      }
-    }
-  },
-
-  /**
-   * Rests the tracked number of clicks on the back button back
-   * to 0.
-   */
-  resetCount() {
-    this._clickCount = 0;
-    this._timerID = null;
-  },
-};
--- browser/components/companion/HistoryCarouselParent.jsm
+++ browser/components/companion/HistoryCarouselParent.jsm
@@ -4,7 +4,120 @@
 
 "use strict";
 
-var EXPORTED_SYMBOLS = ["HistoryCarouselParent"];
+var EXPORTED_SYMBOLS = ["HistoryCarousel", "HistoryCarouselParent"];
+
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+const { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
+
+XPCOMUtils.defineLazyPreferenceGetter(
+  this,
+  "CLICK_COUNT_TIMEOUT_MS",
+  "browser.pinebuild.megaback.click-count-timeout-ms",
+  3000
+);
+
+XPCOMUtils.defineLazyPreferenceGetter(
+  this,
+  "CLICK_COUNT_THRESHOLD",
+  "browser.pinebuild.megaback.click-count-threshold",
+  5
+);
+
+/**
+ * The HistoryCarousel class is responsible for interpreting whether or not
+ * the user has interacted with the back button in a way that indicates
+ * that they want to show the history carousel. Currently, there are
+ * two interactions that do this:
+ *
+ * 1. A long-press on the back button
+ * 2. Frequent clicks on the back button within a time period. Currently,
+ *    that's CLICK_COUNT_THRESHOLD clicks during a
+ *    CLICK_COUNT_TIMEOUT_MS period.
+ */
+class HistoryCarousel {
+  #timerID;
+  #clickCount;
+  #openedByLongPress;
+  #window;
+
+  /**
+   * Sets up the event handlers on the back button.
+   */
+  constructor(window) {
+    this.#timerID = null;
+    this.#clickCount = 0;
+    this.#openedByLongPress = false;
+    this.#window = window;
+  }
+
+  init() {
+    if (
+      !Services.prefs.getBoolPref("browser.pinebuild.megaback.enabled", false)
+    ) {
+      return;
+    }
+    let doc = this.#window.document;
+    let pbBackButton = doc.getElementById("pinebuild-back-button");
+
+    // We need to add this capturing click event handler before adding
+    // to the gClickAndHoldListenersOnElement to ensure the click event
+    // handler is called first. That gives us an opportunity to cancel
+    // the back button click command from running if the user long
+    // presses on the back button.
+    pbBackButton.addEventListener("click", this, { capture: true });
+
+    this.#window.gClickAndHoldListenersOnElement.add(pbBackButton, () => {
+      // The user longpressed, so set this flag to prevent the back
+      // command from being fired on click.
+      this._openedByLongPress = true;
+      this.#window.gGlobalHistory.showHistoryCarousel(true);
+    });
+  }
+
+  handleEvent(event) {
+    if (event.type != "click") {
+      return;
+    }
+    // This is kind of a hack, but if this flag is set, then we
+    // prevent the click event from continuing to propagate, and
+    // call preventDefault. This prevents both the
+    // gClickAndHoldListenersOnElement command and the default
+    // XUL command machinery from firing. We then clear the flag
+    // to make sure that clicks continue to work.
+    if (this.#openedByLongPress) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+      this.#openedByLongPress = false;
+      return;
+    }
+
+    if (!this.#timerID) {
+      this.#clickCount = 1;
+      this.#timerID = this.#window.setTimeout(() => {
+        this.resetCount();
+      }, CLICK_COUNT_TIMEOUT_MS);
+    } else {
+      this.#clickCount++;
+      if (this.#clickCount >= CLICK_COUNT_THRESHOLD) {
+        this.#window.clearTimeout(this._timerID);
+        this.resetCount();
+        this.#window.gGlobalHistory.showHistoryCarousel(true);
+      }
+    }
+  }
+
+  /**
+   * Rests the tracked number of clicks on the back button back
+   * to 0.
+   */
+  resetCount() {
+    this.#clickCount = 0;
+    this.#timerID = null;
+  }
+}
 
 /**
  * HistoryCarouselParent acts as a mediator between an about:historycarousel
