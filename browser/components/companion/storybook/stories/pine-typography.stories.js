fbafbee857cf9fd394f8a7a2af27f658ee09dbf8
Hanna Jones <hjones@mozilla.com>
2022-04-01 14:05:49 +0000

    MR2-2014 - Part 3: Storybook documentation r=mstriemer
    I liked the typography table in Sentry's storybook, so I tried to create something similar: https://storybook.sentry.dev/?path=/story/core-typography--page&globals=backgrounds.value:!hex(333333);backgrounds.grid:false#typography
    
    Differential Revision: https://phabricator.services.mozilla.com/D141527

---
+++ browser/components/companion/storybook/stories/pine-typography.stories.js
@@ -0,0 +1,169 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { html, LitElement, css } from "companion/lit.all.js";
+
+export default {
+  title: "Design System/Foundation/Typography",
+};
+
+const HEADLINE_CLASSES = [
+  "text-headline-l",
+  "text-headline",
+  "text-subheading",
+];
+
+const BODY_CLASSES = [
+  "text-body-s--semi-bold",
+  "text-body-s--med",
+  "text-body-xs--med",
+  "text-body-xxs--med",
+  "text-body-m",
+  "text-body-s",
+  "text-body-xs",
+  "text-body-xxs",
+  "text-body-xxxs",
+];
+
+export const Default = () => html`
+  <link rel="stylesheet" href="chrome://browser/content/companion/fonts.css" />
+  <div>
+    <type-styles-table
+      .classNames=${HEADLINE_CLASSES}
+      .title=${"Headline"}
+    ></type-styles-table>
+    <type-styles-table
+      .classNames=${BODY_CLASSES}
+      .title=${"Body"}
+    ></type-styles-table>
+  </div>
+`;
+
+class TypeStylesTable extends LitElement {
+  static get properties() {
+    return {
+      classNames: { type: Array },
+      title: { type: String },
+      styleProps: { type: Object },
+    };
+  }
+
+  static get styles() {
+    return css`
+      @import url("chrome://browser/content/companion/fonts.css");
+      .container {
+        max-width: 700px;
+        min-width: 500px;
+      }
+
+      table {
+        table-layout: fixed;
+        border-collapse: collapse;
+        border: 1px solid lightgray;
+        text-align: center;
+        width: 100%;
+      }
+
+      tr {
+        border-bottom: 1px solid lightgray;
+      }
+
+      th,
+      td {
+        padding: 0 16px;
+      }
+
+      td {
+        height: 48px;
+      }
+
+      th {
+        height: 36px;
+        font-size: 12px;
+        text-transform: uppercase;
+        font-weight: 600;
+        background: rgba(211, 211, 211, 0.2);
+      }
+
+      th:first-of-type,
+      td:first-of-type {
+        text-align: start;
+        width: 30%;
+      }
+
+      p {
+        margin: 0;
+      }
+    `;
+  }
+
+  constructor() {
+    super();
+    this.styleProps = {};
+    this.stylesTimeout = null;
+  }
+
+  firstUpdated() {
+    this.handleReadStyles();
+  }
+
+  handleReadStyles() {
+    for (let className of this.classNames) {
+      const element = this.renderRoot.querySelector(`.${className}`);
+      const styles = window.getComputedStyle(element);
+      // getting incorrect values for lineheight on storybook startup
+      if (!styles.lineHeight || styles.lineHeight === "normal") {
+        this.stylesTimeout = setTimeout(() => {
+          this.stylesTimeout = null;
+          this.handleReadStyles();
+        }, 100);
+        return;
+      }
+      this.styleProps[className] = {
+        fontSize: styles.fontSize,
+        fontWeight: styles.fontWeight,
+        lineHeight: `${Math.ceil(styles.lineHeight.split("px")[0])}px`,
+      };
+    }
+    this.requestUpdate();
+  }
+
+  render() {
+    return html`
+      <div class="container">
+        <h3>${this.title}</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Class</th>
+              <th>Size</th>
+              <th>Weight</th>
+              <th>Line Height</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${this.classNames.map(
+              className =>
+                html`
+                  <tr>
+                    <td><p class=${className}>${className}</p></td>
+                    <td>
+                      ${this.styleProps[className]?.fontSize || "..."}
+                    </td>
+                    <td>
+                      ${this.styleProps[className]?.fontWeight || "..."}
+                    </td>
+                    <td>
+                      ${this.styleProps[className]?.lineHeight || "..."}
+                    </td>
+                  </tr>
+                `
+            )}
+          </tbody>
+        </table>
+      </div>
+    `;
+  }
+}
+customElements.define("type-styles-table", TypeStylesTable);
