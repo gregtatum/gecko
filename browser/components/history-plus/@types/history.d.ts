9c669344f7b49857e21c1b1e28aee1254b530a62
Greg Tatum <tatum.creative@gmail.com>
2022-05-27 11:00:42 -0500

    Stub out the files for a history-plus component
    This isn't a production ready architecture for a component like this.
    It's mostly a convenient way to to build a prototype. Currently
    everything is loaded to the parent process and can be accessed through
    the chrome url:
    
    chrome://browser/content/history-plus/index.html
    
    Differential Revision: https://phabricator.services.mozilla.com/D151318

---
+++ browser/components/history-plus/@types/history.d.ts
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * This namespace provides a convenient global to refer to types for the HistoryPlus
+ * project. Otherwise these types would need to be manually imported via the verbose:
+ *
+ *   import("../../@types/history.d.ts").TypeName
+ *
+ * Instead the type can be accessed via:
+ *
+ *   HistoryPlus.TypeName
+ */
+declare namespace HistoryPlus {
+
+}
