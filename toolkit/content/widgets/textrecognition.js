ee7fc992a21c71b1231851c615415a8f1ed2f116
Greg Tatum <tatum.creative@gmail.com>
2022-07-05 08:19:44 -0500

    Vendor in DBSCAN from uhho/density-clustering

--- toolkit/content/widgets/textrecognition.js
+++ toolkit/content/widgets/textrecognition.js
@@ -364,3 +364,253 @@ function projectPoints(w, h, x1, y1, x2, y2, x3, y3, x4, y4) {
     mat3[2], mat3[5], 0, mat3[8],
   ];
 }
+
+/**
+ * The MIT License
+ *
+ * Copyright © 2013 Abeja Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the “Software”), to deal in the
+ * Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * The software is provided “as is”, without warranty of any kind, express or
+ * implied, including but not limited to the warranties of merchantability, fitness
+ * for a particular purpose and noninfringement. In no event shall the authors or
+ * copyright holders be liable for any claim, damages or other liability, whether
+ * in an action of contract, tort or otherwise, arising from, out of or in
+ * connection with the software or the use or other dealings in the software.
+ *
+ * DBSCAN - Density based clustering
+ *
+ * https://github.com/uhho/density-clustering/blob/master/lib/DBSCAN.js
+ *
+ * @author Lukasz Krawczyk <contact@lukaszkrawczyk.eu>
+ * @copyright MIT
+ */
+
+class DBSCAN {
+  /**
+   * @param {Array} dataset
+   * @param {number} epsilon
+   * @param {number} minPts
+   * @param {function} distanceFunction
+   * @returns {DBSCAN}
+   */
+  constructor(dataset, epsilon, minPts, distanceFunction) {
+    /** @type {Array} */
+    this.dataset = [];
+    /** @type {number} */
+    this.epsilon = 1;
+    /** @type {number} */
+    this.minPts = 2;
+    /** @type {function} */
+    this.distance = DBSCAN.#euclideanDistance;
+    /** @type {Array} */
+    this.clusters = [];
+    /** @type {Array} */
+    this.noise = [];
+
+    // temporary variables used during computation
+
+    /** @type {Array} */
+    this._visited = [];
+    /** @type {Array} */
+    this._assigned = [];
+    /** @type {number} */
+    this._datasetLength = 0;
+
+    this.#init(dataset, epsilon, minPts, distanceFunction);
+  }
+
+  /**
+   * Start clustering
+   *
+   * @param {Array} dataset
+   * @param {number} epsilon
+   * @param {number} minPts
+   * @param {function} distanceFunction
+   * @returns {undefined}
+   * @access public
+   */
+  run(dataset, epsilon, minPts, distanceFunction) {
+    this.#init(dataset, epsilon, minPts, distanceFunction);
+
+    for (var pointId = 0; pointId < this._datasetLength; pointId++) {
+      // if point is not visited, check if it forms a cluster
+      if (this._visited[pointId] !== 1) {
+        this._visited[pointId] = 1;
+
+        // if closest neighborhood is too small to form a cluster, mark as noise
+        var neighbors = this.#regionQuery(pointId);
+
+        if (neighbors.length < this.minPts) {
+          this.noise.push(pointId);
+        } else {
+          // create new cluster and add point
+          var clusterId = this.clusters.length;
+          this.clusters.push([]);
+          this.#addToCluster(pointId, clusterId);
+
+          this.#expandCluster(clusterId, neighbors);
+        }
+      }
+    }
+
+    return this.clusters;
+  }
+
+  /******************************************************************************/
+  // protected functions
+
+  /**
+   * Set object properties
+   *
+   * @param {Array} dataset
+   * @param {number} epsilon
+   * @param {number} minPts
+   * @param {function} distance
+   * @returns {undefined}
+   * @access protected
+   */
+  #init(dataset, epsilon, minPts, distance) {
+    if (dataset) {
+      if (!(dataset instanceof Array)) {
+        throw Error(
+          "Dataset must be of type array, " + typeof dataset + " given"
+        );
+      }
+
+      this.dataset = dataset;
+      this.clusters = [];
+      this.noise = [];
+
+      this._datasetLength = dataset.length;
+      this._visited = new Array(this._datasetLength);
+      this._assigned = new Array(this._datasetLength);
+    }
+
+    if (epsilon) {
+      this.epsilon = epsilon;
+    }
+
+    if (minPts) {
+      this.minPts = minPts;
+    }
+
+    if (distance) {
+      this.distance = distance;
+    }
+  }
+
+  /**
+   * Expand cluster to closest points of given neighborhood
+   *
+   * @param {number} clusterId
+   * @param {Array} neighbors
+   * @returns {undefined}
+   * @access protected
+   */
+  #expandCluster(clusterId, neighbors) {
+    /**
+     * It's very important to calculate length of neighbors array each time,
+     * as the number of elements changes over time
+     */
+    for (var i = 0; i < neighbors.length; i++) {
+      var pointId2 = neighbors[i];
+
+      if (this._visited[pointId2] !== 1) {
+        this._visited[pointId2] = 1;
+        var neighbors2 = this.#regionQuery(pointId2);
+
+        if (neighbors2.length >= this.minPts) {
+          neighbors = DBSCAN.#mergeArrays(neighbors, neighbors2);
+        }
+      }
+
+      // add to cluster
+      if (this._assigned[pointId2] !== 1) {
+        this.#addToCluster(pointId2, clusterId);
+      }
+    }
+  }
+
+  /**
+   * Add new point to cluster
+   *
+   * @param {number} pointId
+   * @param {number} clusterId
+   */
+  #addToCluster(pointId, clusterId) {
+    this.clusters[clusterId].push(pointId);
+    this._assigned[pointId] = 1;
+  }
+
+  /**
+   * Find all neighbors around given point
+   *
+   * @param {number} pointId,
+   * @param {number} epsilon
+   * @returns {Array}
+   * @access protected
+   */
+  #regionQuery(pointId) {
+    var neighbors = [];
+
+    for (var id = 0; id < this._datasetLength; id++) {
+      var dist = this.distance(this.dataset[pointId], this.dataset[id]);
+      if (dist < this.epsilon) {
+        neighbors.push(id);
+      }
+    }
+
+    return neighbors;
+  }
+
+  /******************************************************************************/
+  // helpers
+
+  /**
+   * @param {Array} a
+   * @param {Array} b
+   * @returns {Array}
+   * @access protected
+   */
+  static #mergeArrays(a, b) {
+    var len = b.length;
+
+    for (var i = 0; i < len; i++) {
+      var P = b[i];
+      if (!a.includes(P)) {
+        a.push(P);
+      }
+    }
+
+    return a;
+  }
+
+  /**
+   * Calculate euclidean distance in multidimensional space
+   *
+   * @param {Array} p
+   * @param {Array} q
+   * @returns {number}
+   * @access protected
+   */
+  static #euclideanDistance(p, q) {
+    var sum = 0;
+    var i = Math.min(p.length, q.length);
+
+    while (i--) {
+      sum += (p[i] - q[i]) * (p[i] - q[i]);
+    }
+
+    return Math.sqrt(sum);
+  }
+}
