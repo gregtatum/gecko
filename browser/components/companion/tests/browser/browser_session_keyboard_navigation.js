a368745cfb5a95dfc17b54e6d2cd236f32ccf821
Tim Giles <tgiles@mozilla.com>
2022-07-20 15:37:19 +0000

    MR2-2309 - Make "Browse" control of companion tab focusable. r=jhirsch
    Differential Revision: https://phabricator.services.mozilla.com/D151722

--- browser/components/companion/tests/browser/browser_session_keyboard_navigation.js
+++ browser/components/companion/tests/browser/browser_session_keyboard_navigation.js
@@ -121,3 +121,55 @@ add_task(async function test_last_session_card_keyboard_navigation() {
   }, win);
   await BrowserTestUtils.closeWindow(win);
 });
+
+/**
+ * Ensure that the "Browse" link in the "Now" section is usable by a keyboard.
+ * This link appears in the "Start fresh" illustration after setting aside a session.
+ * */
+add_task(async function test_browse_control_after_setting_aside_session() {
+  // Run test in a new window to avoid affecting the main test window.
+  let win = await BrowserTestUtils.openNewBrowserWindow({
+    waitForTabURL: "about:flow-reset",
+  });
+  // Visit a page to create a session.
+  BrowserTestUtils.loadURI(
+    win.gBrowser.selectedBrowser,
+    "https://example.com/"
+  );
+  await BrowserTestUtils.browserLoaded(
+    win.gBrowser.selectedBrowser,
+    false,
+    "https://example.com/"
+  );
+  // Set aside session so we get the option to restore our last session.
+  await PinebuildTestUtils.setAsideSession(win);
+
+  await CompanionHelper.whenReady(async helper => {
+    await helper.runCompanionTask(async () => {
+      let flowResetClose = content.document.getElementById("flow-reset-close");
+      flowResetClose.focus();
+      is(
+        content.document.activeElement,
+        flowResetClose,
+        "Flow reset close button should be focused"
+      );
+      EventUtils.synthesizeKey("KEY_Tab", {}, content);
+      let browseControlLink = content.document.getElementById(
+        "session-cleared-link"
+      );
+      is(
+        content.document.activeElement,
+        browseControlLink,
+        "Browse link in the companion should be focused"
+      );
+      EventUtils.synthesizeKey("KEY_Enter", {}, content);
+      let namedDeck = content.document.querySelector("named-deck");
+      is(
+        namedDeck.selectedViewName,
+        "browse",
+        `The current deck view is "browse"`
+      );
+    });
+  }, win);
+  await BrowserTestUtils.closeWindow(win);
+});
