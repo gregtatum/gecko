d7074f1a792d06a9894f50c8f406e060a010f71a
Mike Conley <mconley@mozilla.com>
2022-08-25 11:24:01 -0400

    Backed out changeset a93dd6de6cfe for mochitest-browser failures.

--- browser/components/companion/content/components/view-group-element.js
+++ browser/components/companion/content/components/view-group-element.js
@@ -198,26 +198,24 @@ export default class ViewGroupElement extends MozLitElement {
     let history = [];
     let viewGroupHistory = this.computeHistoryVisualization(view);
 
-    if (viewGroupHistory.length > 1) {
-      for (let { view: historyView, size } of viewGroupHistory) {
-        let classes = {
-          history: true,
-          active: this.active && historyView == this.activeView,
-        };
+    for (let { view: historyView, size } of viewGroupHistory) {
+      let classes = {
+        history: true,
+        active: this.active && historyView == this.activeView,
+      };
 
-        history.push(
-          html`
-            <button
-              class=${classMap(classes)}
-              .view=${historyView}
-              @click=${this.#onHistoryItemSelected}
-              @keyup=${this.#onViewHistoryNavigation}
-              title=${historyView.title}
-              size=${size}
-            ></button>
-          `
-        );
-      }
+      history.push(
+        html`
+          <button
+            class=${classMap(classes)}
+            .view=${historyView}
+            @click=${this.#onHistoryItemSelected}
+            @keyup=${this.#onViewHistoryNavigation}
+            title=${historyView.title}
+            size=${size}
+          ></button>
+        `
+      );
     }
 
     // If we're not active, then we want the entire ViewGroup to show
@@ -268,15 +266,15 @@ export default class ViewGroupElement extends MozLitElement {
           <div class="view-title"
                part="title"
                title=${view.title}>${view.title}</div>
-          <div class="view-history" part=${
-            shouldExposeParts ? "history" : ""
-          }>${history}</div>
           <div class="view-domain-container" part=${
             shouldExposeParts ? "domain" : ""
           }>
             <div id="view-security-icon" class="${securityIconClass}"></div>
-            <input class="view-domain" type="text" readonly value=${domain} tabindex="-1"></input>
+            <input class="view-domain" type="text" readonly value=${domain}></input>
           </div>
+          <div class="view-history" part=${
+            shouldExposeParts ? "history" : ""
+          }>${history}</div>
         </div>
         <button class="page-action-button" part="page-action-button" ?hidden=${!this
           .active}
--- browser/components/companion/tests/browser/browser_ViewGroupElement_history_visibility.js
+++ browser/components/companion/tests/browser/browser_ViewGroupElement_history_visibility.js
@@ -4,31 +4,6 @@
 "use strict";
 
 /**
- * When the breadcrumbs are "hidden", they're not actually fully
- * hidden - they're _nearly_ hidden with a low opacity in order to
- * allow them to still be keyboard focusable (which snaps them into
- * visibility). This means that we cannot use
- * BrowserTestUtils.is_hidden to test the visibility of the breadcrumbs
- * when they're "hidden". This helper function does that work
- * for us.
- *
- * @param {Element} historyEl
- *   The DOM node that contains the breadcrumbs for a ViewGroupElement.
- * @param {boolean} isVisible
- *   True if it's expected that the history items be fully visible. False
- *   if they should be "hidden".
- */
-function assertBreadcrumbsVisible(historyEl, isVisible) {
-  let style = window.getComputedStyle(historyEl);
-  let opacity = parseFloat(style.opacity);
-  if (isVisible) {
-    Assert.equal(opacity, 1, "History items are visible.");
-  } else {
-    Assert.ok(opacity < 0.01, "History items are (effectively) hidden.");
-  }
-}
-
-/**
  * Tests that breadcrumbs are visible only when hovering the main toolbar,
  * or if the user is keyboard navigating through the ViewGroupElement.
  */
@@ -51,8 +26,10 @@ add_task(async function test_ViewGroupElement_visibility() {
   await viewGroupEl.updateComplete;
   let history = viewGroupEl.shadowRoot.querySelector("[part=history]");
   let domain = viewGroupEl.shadowRoot.querySelector("[part=domain]");
-
-  assertBreadcrumbsVisible(history, false);
+  Assert.ok(
+    BrowserTestUtils.is_hidden(history),
+    "History breadcrumbs should be hidden."
+  );
   Assert.ok(!BrowserTestUtils.is_hidden(domain), "Domain should be visible.");
 
   // Now keyboard focus into the ViewGroupElement...
@@ -67,28 +44,28 @@ add_task(async function test_ViewGroupElement_visibility() {
   let river = document.querySelector("river-el");
   await river.updateComplete;
 
-  // The breadcrumbs should not yet be visible, since we haven't keyboard focused
-  // to them yet.
-  assertBreadcrumbsVisible(history, false);
-  Assert.ok(!BrowserTestUtils.is_hidden(domain), "Domain should be visible.");
-
-  // Okay, now tab into the breadcrumbs. This should make the breadcrumbs visible.
-  EventUtils.synthesizeKey("VK_TAB", {});
-
-  assertBreadcrumbsVisible(history, true);
+  Assert.ok(
+    !BrowserTestUtils.is_hidden(history),
+    "History breadcrumbs should be visible."
+  );
   Assert.ok(BrowserTestUtils.is_hidden(domain), "Domain should be hidden.");
 
   // Now move focus back into the content area.
   browser.focus();
   await river.updateComplete;
-
-  assertBreadcrumbsVisible(history, false);
+  Assert.ok(
+    BrowserTestUtils.is_hidden(history),
+    "History breadcrumbs should be hidden."
+  );
   Assert.ok(!BrowserTestUtils.is_hidden(domain), "Domain should be visible.");
 
   // Now hover the main toolbar with the mouse.
   let navBar = document.getElementById("nav-bar");
   EventUtils.synthesizeMouseAtCenter(navBar, { type: "mousemove" });
-  assertBreadcrumbsVisible(history, true);
+  Assert.ok(
+    !BrowserTestUtils.is_hidden(history),
+    "History breadcrumbs should be visible."
+  );
   Assert.ok(BrowserTestUtils.is_hidden(domain), "Domain should be hidden.");
 
   // Click on the first breadcrumb now.
@@ -99,39 +76,9 @@ add_task(async function test_ViewGroupElement_visibility() {
 
   EventUtils.synthesizeMouseAtCenter(browser, { type: "mousemove" });
   await river.updateComplete;
-  assertBreadcrumbsVisible(history, false);
-  Assert.ok(!BrowserTestUtils.is_hidden(domain), "Domain should be visible.");
-  await gStageManager.reset();
-});
-
-/**
- * Tests that breadcrumbs don't stay visible after the Page Action Menu closes
- * due to a user action.
- */
-add_task(async function test_breadcrumb_visibility_after_PAM() {
-  await PinebuildTestUtils.loadViews([
-    "https://example.com/",
-    "https://example.com/browser/",
-    "https://example.com/browser/components",
-  ]);
-  let viewGroupEls = await PinebuildTestUtils.getViewGroupEls(window);
-  Assert.equal(viewGroupEls.length, 1, "There should be one total ViewGroups");
-
-  let viewGroupEl = viewGroupEls[0];
-
-  let history = viewGroupEl.shadowRoot.querySelector("[part=history]");
-  let domain = viewGroupEl.shadowRoot.querySelector("[part=domain]");
-
-  assertBreadcrumbsVisible(history, false);
-  Assert.ok(!BrowserTestUtils.is_hidden(domain), "Domain should be visible.");
-
-  let pam = await PinebuildTestUtils.openPageActionMenu(viewGroupEls[0]);
-  let copyButton = pam.querySelector("#page-action-copy-url");
-
-  let pamClosed = BrowserTestUtils.waitForEvent(pam, "popuphidden");
-  EventUtils.synthesizeMouseAtCenter(copyButton, {}, window);
-  await pamClosed;
-
-  assertBreadcrumbsVisible(history, false);
+  Assert.ok(
+    BrowserTestUtils.is_hidden(history),
+    "History breadcrumbs should be hidden."
+  );
   Assert.ok(!BrowserTestUtils.is_hidden(domain), "Domain should be visible.");
 });
