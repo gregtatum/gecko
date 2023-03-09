61c70e2cf0dbf372b52a0559a6e58c4e764fed27
Greg Tatum <tatum.creative@gmail.com>
2023-03-09 08:29:37 -0600

    Add a trace logging level

--- toolkit/components/translations/actors/TranslationsChild.sys.mjs
+++ toolkit/components/translations/actors/TranslationsChild.sys.mjs
@@ -262,8 +262,7 @@ export class TranslationsEngine {
       enginePayload,
       innerWindowId,
       messageId: this.#messageId++,
-      isLoggingEnabled:
-        Services.prefs.getCharPref("browser.translations.logLevel") === "All",
+      logLevel: Services.prefs.getCharPref("browser.translations.logLevel"),
     });
   }
 
