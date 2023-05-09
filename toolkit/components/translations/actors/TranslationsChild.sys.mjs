753836ca08b7ccbd1ed474ed71b194e961a0fe41
Greg Tatum <tatum.creative@gmail.com>
2023-05-09 10:53:42 -0500

    Bug 1820249 - Apply UX feedback to translations icon; r?gregtatum

--- toolkit/components/translations/actors/TranslationsChild.sys.mjs
+++ toolkit/components/translations/actors/TranslationsChild.sys.mjs
@@ -829,6 +829,8 @@ export class TranslationsChild extends JSWindowActorChild {
       this.contentWindow.location.href
     );
 
+    this.sendAsyncMessage("Translations:EngineIsReady");
+
     this.translatedDoc.addRootElement(this.document.querySelector("title"));
     this.translatedDoc.addRootElement(
       this.document.body,
--- toolkit/components/translations/actors/TranslationsParent.sys.mjs
+++ toolkit/components/translations/actors/TranslationsParent.sys.mjs
@@ -315,6 +315,10 @@ export class TranslationsParent extends JSWindowActorParent {
       case "Translations:GetLanguagePairs": {
         return this.getLanguagePairs();
       }
+      case "Translations:EngineIsReady":
+        this.isEngineReady = true;
+        this.languageState.isEngineReady = true;
+        break;
       case "Translations:MaybeAutoTranslate": {
         if (!lazy.autoTranslatePagePref) {
           return false;
@@ -1426,6 +1430,8 @@ class TranslationsLanguageState {
   /** @type {null | TranslationErrors} */
   #error = null;
 
+  #isEngineReady = false;
+
   /**
    * Dispatch anytime the language details change, so that any UI can react to it.
    */
@@ -1447,6 +1453,7 @@ class TranslationsLanguageState {
           detectedLanguages: this.#detectedLanguages,
           requestedTranslationPair: this.#requestedTranslationPair,
           error: this.#error,
+          isEngineReady: this.#isEngineReady,
         },
       })
     );
@@ -1465,6 +1472,7 @@ class TranslationsLanguageState {
 
   set requestedTranslationPair(requestedTranslationPair) {
     this.#error = null;
+    this.#isEngineReady = false;
     this.#requestedTranslationPair = requestedTranslationPair;
     this.dispatch();
   }
@@ -1515,6 +1523,19 @@ class TranslationsLanguageState {
     this.#error = error;
     // Setting an error invalidates the requested translation pair.
     this.#requestedTranslationPair = null;
+    this.#isEngineReady = false;
+    this.dispatch();
+  }
+
+  /**
+   * The last error that occured during translation.
+   */
+  get isEngineReady() {
+    return this.#isEngineReady;
+  }
+
+  set isEngineReady(isEngineReady) {
+    this.#isEngineReady = isEngineReady;
     this.dispatch();
   }
 }
