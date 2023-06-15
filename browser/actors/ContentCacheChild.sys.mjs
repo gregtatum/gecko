88b08ea18d97f0d4b2703ba8716110646df25983
Greg Tatum <tatum.creative@gmail.com>
2023-06-15 15:34:53 -0500

    Fix language detector

--- browser/actors/ContentCacheChild.sys.mjs
+++ browser/actors/ContentCacheChild.sys.mjs
@@ -1,16 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-"use strict";
-
-const { XPCOMUtils } = ChromeUtils.importESModule(
-  "resource://gre/modules/XPCOMUtils.sys.mjs"
-);
 
 const lazy = {};
 
-XPCOMUtils.defineLazyModuleGetters(lazy, {
-  LanguageDetector: "resource://gre/modules/translation/LanguageDetector.jsm",
+ChromeUtils.defineESModuleGetters(lazy, {
+  LanguageDetector:
+    "resource://gre/modules/translation/LanguageDetector.sys.mjs",
 });
 
 /**
