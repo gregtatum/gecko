a37b5709608e8ca78d0de6755d0b0567145d272e
Greg Tatum <tatum.creative@gmail.com>
2023-06-21 09:48:17 -0500

    Bug 1835406 - Fix translations button focus on restoring page; r?nordzilla!

--- toolkit/components/translations/actors/TranslationsParent.sys.mjs
+++ toolkit/components/translations/actors/TranslationsParent.sys.mjs
@@ -148,8 +148,18 @@ export class TranslationsParent extends JSWindowActorParent {
    */
   #isDestroyed = false;
 
+  /**
+   * Remember the detected languages on a page reload.
+   * @type {LangTags | null} previousDetectedLanguages
+   */
+  static #previousDetectedLanguages = null;
+
   actorCreated() {
-    this.languageState = new TranslationsLanguageState(this);
+    this.languageState = new TranslationsLanguageState(
+      this,
+      TranslationsParent.#previousDetectedLanguages
+    );
+    TranslationsParent.#previousDetectedLanguages = null;
 
     if (TranslationsParent.#translateOnPageReload) {
       // The actor was recreated after a page reload, start the translation.
@@ -1639,6 +1649,8 @@ export class TranslationsParent extends JSWindowActorParent {
       TranslationsParent.#isPageRestoredForAutoTranslate = true;
     }
     this.languageState.requestedTranslationPair = null;
+    TranslationsParent.#previousDetectedLanguages =
+      this.languageState.detectedLanguages;
 
     const browser = this.browsingContext.embedderElement;
     browser.reload();
@@ -2022,9 +2034,11 @@ function detectSimdSupport() {
 class TranslationsLanguageState {
   /**
    * @param {TranslationsParent} actor
+   * @param {LangTags | null} previousDetectedLanguages
    */
-  constructor(actor) {
+  constructor(actor, previousDetectedLanguages = null) {
     this.#actor = actor;
+    this.#detectedLanguages = previousDetectedLanguages;
     this.dispatch();
   }
 
