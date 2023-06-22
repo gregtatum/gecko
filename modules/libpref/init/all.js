7f8887bd85ebac224be0c08c14be8f1ded9a6754
Greg Tatum <tatum.creative@gmail.com>
2023-06-22 15:28:31 -0500

    Bug 1838665 - Automatically pop up the panel when a translation is available; r?nordzilla!
    Differential Revision: https://phabricator.services.mozilla.com/D181913

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3670,6 +3670,8 @@ pref("browser.translations.useHTML", false);
 // so that the page automatically performs a translation if one is detected as being
 // required.
 pref("browser.translations.autoTranslate", false);
+// Automatically popup an offer to translate on sites.
+pref("browser.translations.automaticallyPopup", true);
 // Simulate the behavior of using a device that does not support the translations engine.
 // Requires restart.
 pref("browser.translations.simulateUnsupportedEngine", false);
