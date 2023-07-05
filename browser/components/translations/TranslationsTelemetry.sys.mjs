37d7e764579bf8893a8f9f089927db3cca4554d3
Erik Nordin <enordin@mozilla.com>
2023-07-05 17:42:20 -0500

    Bug 1841366 - Add flowId to existing Translations telemetry events r=gregtatum!
    Instruments all pre-existing Translations telemetry  events
    to utilize the flowId.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182612
    
    Depends on D182611

--- browser/components/translations/TranslationsTelemetry.sys.mjs
+++ browser/components/translations/TranslationsTelemetry.sys.mjs
@@ -2,6 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const lazy = {};
+
+ChromeUtils.defineESModuleGetters(lazy, {
+  // The shared TranslationsTelemetry across all of toolkit
+  TranslationsTelemetry:
+    "chrome://global/content/translations/TranslationsTelemetry.sys.mjs",
+});
+
 /**
  * Telemetry functions for Translations desktop UI
  */
@@ -13,6 +21,7 @@ export class TranslationsTelemetry {
    */
   static onOpenPanel(openedFromAppMenu) {
     Glean.translationsPanel.open.record({
+      flow_id: lazy.TranslationsTelemetry.createFlowId(),
       opened_from: openedFromAppMenu ? "appMenu" : "translationsButton",
     });
   }
