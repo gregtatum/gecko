a97d05db8797ec27fa51906f8cb8b4a082311b63
Greg Tatum <tatum.creative@gmail.com>
2023-07-25 10:26:34 -0500

    Bug 1820261 - Enable translations for release; r?nordzilla!
    Differential Revision: https://phabricator.services.mozilla.com/D184488

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1852,9 +1852,7 @@ pref("browser.translation.neverForLanguages", "");
 
 // Enable Firefox translations powered by the Bergamot translations
 // engine https://browser.mt/.
-#ifdef NIGHTLY_BUILD
 pref("browser.translations.enable", true);
-#endif
 
 // Telemetry settings.
 // Determines if Telemetry pings can be archived locally.
