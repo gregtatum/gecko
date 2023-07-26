98861513866fcaffaf83e12380dd3d9be0812164
Greg Tatum <tatum.creative@gmail.com>
2023-07-26 08:41:05 -0500

    Bug 1845518 - Disable the translation automatic offering in automation; r?nordzilla!
    Differential Revision: https://phabricator.services.mozilla.com/D184579

--- testing/marionette/client/marionette_driver/geckoinstance.py
+++ testing/marionette/client/marionette_driver/geckoinstance.py
@@ -45,6 +45,8 @@ class GeckoInstance(object):
         "browser.region.network.url": "",
         # Don't pull Top Sites content from the network
         "browser.topsites.contile.enabled": False,
+        # Do not automatically offer translations, as tests do not expect this.
+        "browser.translations.automaticallyPopup": False,
         # Disable UI tour
         "browser.uitour.pinnedTabUrl": "http://%(server)s/uitour-dummy/pinnedTab",
         "browser.uitour.url": "http://%(server)s/uitour-dummy/tour",
