93d2993512b520c42ad712477cb34ea0e1974344
Erik Nordin <nordzilla@mozilla.com>
2023-08-30 11:12:18 -0500

    Bug 1850303 - Refactor click cancel button r=gregtatum!
    Refactors clicking the cancel button to be more
    declarative within the Translations test suite.
    
    Differential Revision: https://phabricator.services.mozilla.com/D186904
    
    Depends on D186903

--- browser/components/translations/tests/browser/head.js
+++ browser/components/translations/tests/browser/head.js
@@ -378,6 +378,14 @@ async function assertLangTagIsShownOnTranslationsButton(
   );
 }
 
+async function clickCancelButton() {
+  const { cancelButton } = TranslationsPanel.elements;
+  ok(isVisible(cancelButton), "Expected the cancel button to be visible");
+  await waitForTranslationsPopupEvent("popuphidden", () => {
+    click(cancelButton, "Clicking the cancel button");
+  });
+}
+
 /**
  * Asserts that for each provided expectation, the visible state of the corresponding
  * element in TranslationsPanel.elements both exists and matches the visibility expectation.
@@ -712,7 +720,6 @@ async function waitForTranslationsPopupEvent(
   }
   const promise = BrowserTestUtils.waitForEvent(panel, eventName);
   await callback();
-  info("Waiting for the translations panel popup to be shown");
   await promise;
   if (postEventAssertion) {
     postEventAssertion();
