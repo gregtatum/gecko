bc4d8008003d558f17e4e72a4d5a38f2a0220ebf
Erik Nordin <nordzilla@mozilla.com>
2023-08-30 11:12:28 -0500

    Bug 1850303 - Refactor click translate button r=gregtatum!
    Refactors clicking the translate button to be more
    declarative within the Translations test suite.
    
    Differential Revision: https://phabricator.services.mozilla.com/D186907
    
    Depends on D186906

--- browser/components/translations/tests/browser/head.js
+++ browser/components/translations/tests/browser/head.js
@@ -405,6 +405,24 @@ async function clickDismissErrorButton() {
   });
 }
 
+async function clickTranslateButton({
+  downloadHandler = null,
+  pivotTranslation = false,
+} = {}) {
+  const { translateButton } = TranslationsPanel.elements;
+  ok(isVisible(translateButton), "Expect the translate button to be visible");
+  await waitForTranslationsPopupEvent("popuphidden", () => {
+    click(translateButton, "Click the translate button");
+  });
+  await assertTranslationsButton(
+    { button: true, circleArrows: true, locale: false, icon: true },
+    "The icon presents the loading indicator."
+  );
+  if (downloadHandler) {
+    await downloadHandler(pivotTranslation ? 2 : 1);
+  }
+}
+
 /**
  * Asserts that for each provided expectation, the visible state of the corresponding
  * element in TranslationsPanel.elements both exists and matches the visibility expectation.
