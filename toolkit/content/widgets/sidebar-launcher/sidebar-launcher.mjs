2ecf37390033244edb4bdef51e291b1eb9e81fe4
Mark Striemer <mstriemer@mozilla.com>
2024-01-24 19:09:27 +0000

    Add sidebar tabs, example cards in Now/Home sections
    Differential Revision: https://phabricator.services.mozilla.com/D199554

--- toolkit/content/widgets/sidebar-launcher/sidebar-launcher.mjs
+++ toolkit/content/widgets/sidebar-launcher/sidebar-launcher.mjs
@@ -6,7 +6,17 @@ import { html, styleMap } from "../vendor/lit.all.mjs";
 import { MozLitElement } from "../lit-utils.mjs";
 
 // eslint-disable-next-line
-import "chrome://global/content/elements/moz-button.mjs";
+// import "chrome://global/content/elements/moz-button.mjs";
+
+const TAB_EVENTS = new Set([
+  "TabSelect",
+  "TabAttrModified",
+  "TabClose",
+  "TabMove",
+  "TabOpen",
+  "TabPinned",
+  "TabUnpinned",
+]);
 
 /**
  * Component description goes here.
@@ -20,6 +30,11 @@ export default class SidebarLauncher extends MozLitElement {
     bottomActions: { type: Array },
     selectedView: { type: String },
     open: { type: Boolean },
+    tabs: { type: Array },
+  };
+
+  static queries = {
+    selectedTab: ".open-tabs button[selected]",
   };
 
   constructor() {
@@ -56,6 +71,7 @@ export default class SidebarLauncher extends MozLitElement {
 
     this.selectedView = window.SidebarUI.currentID;
     this.open = window.SidebarUI.isOpen;
+    this.tabs = window.gBrowser.tabs;
   }
 
   connectedCallback() {
@@ -64,15 +80,46 @@ export default class SidebarLauncher extends MozLitElement {
     this._sidebarBox.addEventListener("sidebar-show", this);
     this._sidebarBox.addEventListener("sidebar-hide", this);
     this._sidebarMenu = document.getElementById("viewSidebarMenu");
-    let menuMutationObserver = new MutationObserver(() => this.#setExtensionItems());
-    menuMutationObserver.observe(this._sidebarMenu, { childList: true, subtree: true });
+    let menuMutationObserver = new MutationObserver(() =>
+      this.#setExtensionItems()
+    );
+    menuMutationObserver.observe(this._sidebarMenu, {
+      childList: true,
+      subtree: true,
+    });
     this.#setExtensionItems();
+    for (let eventName of TAB_EVENTS.values()) {
+      window.gBrowser.tabContainer.addEventListener(eventName, this);
+    }
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
     this._sidebarBox.removeEventListener("sidebar-show", this);
     this._sidebarBox.removeEventListener("sidebar-hide", this);
+    for (let eventName of TAB_EVENTS.values()) {
+      window.gBrowser.tabContainer.removeEventListener(eventName, this);
+    }
+  }
+
+  getImageUrl(icon, targetURI) {
+    if (window.IS_STORYBOOK) {
+      return `chrome://global/skin/icons/defaultFavicon.svg`;
+    }
+    if (!icon) {
+      if (targetURI?.startsWith("moz-extension")) {
+        return "chrome://mozapps/skin/extensions/extension.svg";
+      }
+      return `chrome://global/skin/icons/defaultFavicon.svg`;
+    }
+    // If the icon is not for website (doesn't begin with http), we
+    // display it directly. Otherwise we go through the page-icon
+    // protocol to try to get a cached version. We don't load
+    // favicons directly.
+    if (icon.startsWith("http")) {
+      return `page-icon:${targetURI}`;
+    }
+    return icon;
   }
 
   #setExtensionItems() {
@@ -89,6 +136,13 @@ export default class SidebarLauncher extends MozLitElement {
   }
 
   handleEvent(e) {
+    if (TAB_EVENTS.has(e.type)) {
+      this.tabSelectEvent = this.tabSelectEvent || e.type == "TabSelect" || e.type == "TabOpen";
+      this.tabs = window.gBrowser.tabs;
+      this.requestUpdate();
+      return;
+    }
+
     switch (e.type) {
       case "sidebar-show":
         this.selectedView = e.detail.viewId;
@@ -100,12 +154,21 @@ export default class SidebarLauncher extends MozLitElement {
     }
   }
 
+  updated() {
+    if (this.tabSelectEvent) {
+      this.selectedTab.scrollIntoView({ block: "nearest", behavior: "smooth" });
+    }
+    this.tabSelectEvent = false;
+  }
+
   showView(e) {
     window.SidebarUI.toggle(e.target.getAttribute("view"));
   }
 
   buttonType(action) {
-    return this.open && action.view == this.selectedView ? "icon" : "icon ghost";
+    return this.open && action.view == this.selectedView
+      ? "icon"
+      : "icon ghost";
   }
 
   render() {
@@ -114,29 +177,64 @@ export default class SidebarLauncher extends MozLitElement {
         rel="stylesheet"
         href="chrome://global/content/elements/sidebar-launcher.css"
       />
+      <link
+        rel="stylesheet"
+        href="chrome://global/skin/in-content/common.css"
+      />
       <div class="wrapper">
         <div class="top-actions">
           ${this.topActions.map(
             action =>
-              html`<moz-button
+              html`<button
+                class="ghost-button icon-button"
+                ?selected=${this.open && action.view == this.selectedView}
                 type=${this.buttonType(action)}
                 @click=${this.showView}
                 view=${action.view}
                 data-l10n-id=${action.l10nId}
                 style=${styleMap({ "--action-icon": action.icon })}
-              ></moz-button>`
+              ></button>`
           )}
         </div>
-        <div class="bottom-actions">
+        <div class="open-tabs">
+          ${this.tabs.map(
+            t => html`
+              <button
+                class="ghost-button icon-button"
+                ?selected=${t.selected}
+                @click=${() => (gBrowser.selectedTab = t)}
+                title=${t.label}
+                style=${styleMap({
+                  "--action-icon": `url("${this.getImageUrl(
+                    t.getAttribute("image"),
+                    t.linkedBrowser?.currentURI?.spec
+                  )}")`,
+                })}
+              ></button>
+            `
+          )}
+        </div>
+        <button
+          class="ghost-button icon-button"
+          @click=${event => BrowserOpenTab({ event })}
+          title="Open a new tab"
+          style=${styleMap({
+            "--action-icon": `url(chrome://global/skin/icons/plus.svg)`,
+          })}
+        ></button>
+        <div class="bottom-actions" ?empty=${!this.bottomActions.length}>
           ${this.bottomActions.map(
             action =>
-              html`<moz-button
+              html`<button
+                class="ghost-button icon-button"
+                ?selected=${this.open && action.view == this.selectedView}
                 type=${this.buttonType(action)}
                 @click=${this.showView}
                 view=${action.view}
                 .tooltiptext=${action.tooltiptext}
+                title=${action.tooltiptext}
                 style=${styleMap({ "--action-icon": action.icon })}
-              ></moz-button>`
+              ></button>`
           )}
         </div>
       </div>
