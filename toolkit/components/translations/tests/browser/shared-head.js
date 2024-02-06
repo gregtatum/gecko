9e1121318efac9fee9893a90bd6975fb28c4c41a
Erik Nordin <enordin@mozilla.com>
2024-02-06 15:41:40 -0600

    WIP: Bug 1870327 - Rename fullPageTranslationsPanel ids
    Differential Revision: https://phabricator.services.mozilla.com/D200876

--- toolkit/components/translations/tests/browser/shared-head.js
+++ toolkit/components/translations/tests/browser/shared-head.js
@@ -383,14 +383,14 @@ async function closeContextMenuIfOpen() {
  * Closes the translations panel settings menu if it is open.
  */
 async function closeSettingsMenuIfOpen() {
-  await closePopupIfOpen("translations-panel-settings-menupopup");
+  await closePopupIfOpen("full-page-translations-panel-settings-menupopup");
 }
 
 /**
  * Closes the translations panel if it is open.
  */
 async function closeTranslationsPanelIfOpen() {
-  await closePopupIfOpen("translations-panel");
+  await closePopupIfOpen("full-page-translations-panel");
 }
 
 /**
@@ -543,7 +543,7 @@ async function loadTestPage({
   if (autoOffer && TranslationsParent.shouldAlwaysOfferTranslations()) {
     info("Waiting for the popup to be automatically shown.");
     await waitForCondition(() => {
-      const panel = document.getElementById("translations-panel");
+      const panel = document.getElementById("full-page-translations-panel");
       return panel && panel.state === "open";
     });
   }
