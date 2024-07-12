443e1ad993d776b367247a985f0b264e623bdaa4
Erik Nordin <enordin@mozilla.com>
2024-07-12 12:39:30 -0500

    Bug 1904355 - Hide translate-full-page button when full-page translations is active r=#translations-reviewers!
    Now that Select Translations is available while Full-Page-Translations (FPT)
    is active, this patch ensures that the translate-full-page button in the
    Select Translations Panel is hidden and disabled when FPT is currently active,
    to avoid complicated UX around what it would mean to re-trigger a FPT request.
    
    Differential Revision: https://phabricator.services.mozilla.com/D216424

--- browser/components/translations/tests/browser/head.js
+++ browser/components/translations/tests/browser/head.js
@@ -200,6 +200,24 @@ function logAction(...params) {
 }
 
 /**
+ * Returns true if Full-Page Translations is currently active, otherwise false.
+ *
+ * @returns {boolean}
+ */
+function isFullPageTranslationsActive() {
+  try {
+    const { requestedTranslationPair } =
+      TranslationsParent.getTranslationsActor(
+        gBrowser.selectedBrowser
+      ).languageState;
+    return !!requestedTranslationPair;
+  } catch {
+    // Translations actor unavailable, continue on.
+  }
+  return false;
+}
+
+/**
  * Navigate to a URL and indicate a message as to why.
  */
 async function navigate(
@@ -1660,14 +1678,20 @@ class SelectTranslationsTestUtils {
       textArea: true,
       toLabel: true,
       toMenuList: true,
-      translateFullPageButton: !isFullPageTranslationsRestrictedForPage,
+      translateFullPageButton: !(
+        isFullPageTranslationsRestrictedForPage ||
+        isFullPageTranslationsActive()
+      ),
     });
     SelectTranslationsTestUtils.#assertConditionalUIEnabled({
       copyButton: true,
       doneButtonPrimary: true,
       textArea: true,
-      translateFullPageButton:
-        !sameLanguageSelected && !isFullPageTranslationsRestrictedForPage,
+      translateFullPageButton: !(
+        sameLanguageSelected ||
+        isFullPageTranslationsRestrictedForPage ||
+        isFullPageTranslationsActive()
+      ),
     });
 
     await waitForCondition(
@@ -1683,7 +1707,11 @@ class SelectTranslationsTestUtils {
     await SelectTranslationsTestUtils.#assertPanelTextAreaOverflow();
 
     let footerButtons;
-    if (sameLanguageSelected || isFullPageTranslationsRestrictedForPage) {
+    if (
+      sameLanguageSelected ||
+      isFullPageTranslationsRestrictedForPage ||
+      isFullPageTranslationsActive()
+    ) {
       footerButtons = [copyButton, doneButtonPrimary];
     } else {
       footerButtons =
@@ -1914,7 +1942,10 @@ class SelectTranslationsTestUtils {
       textArea: true,
       toLabel: true,
       toMenuList: true,
-      translateFullPageButton: !isFullPageTranslationsRestrictedForPage,
+      translateFullPageButton: !(
+        isFullPageTranslationsRestrictedForPage ||
+        isFullPageTranslationsActive()
+      ),
     });
     SelectTranslationsTestUtils.#assertPanelHasTranslatingPlaceholder();
   }
@@ -1943,7 +1974,8 @@ class SelectTranslationsTestUtils {
       doneButtonPrimary: true,
       translateFullPageButton:
         fromMenuList.value !== toMenuList.value &&
-        !isFullPageTranslationsRestrictedForPage,
+        !isFullPageTranslationsRestrictedForPage &&
+        !isFullPageTranslationsActive(),
     });
   }
 
@@ -1965,7 +1997,9 @@ class SelectTranslationsTestUtils {
       copyButton: true,
       doneButtonPrimary: true,
       translateFullPageButton:
-        fromLanguage !== toLanguage && !isFullPageTranslationsRestrictedForPage,
+        fromLanguage !== toLanguage &&
+        !isFullPageTranslationsRestrictedForPage &&
+        !isFullPageTranslationsActive(),
     });
 
     if (fromLanguage === toLanguage) {
