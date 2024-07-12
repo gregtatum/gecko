75825f20d383856d97f9977d6563848486a6eb3e
Erik Nordin <enordin@mozilla.com>
2024-07-12 12:39:33 -0500

    Bug 1904355 - Ensure Select Translations errors are decouple from Full Page r=#translations-reviewers!
    The Full-Page Translations (FPT) actor listens for translation errors
    tied to the innerWindowID when it is active. This way it can show the
    FPT panel error state if an error occurs. This patch ensures that the
    Select Translations Panel (STP) is decoupled from the innerWindowID,
    so that if the STP encounters a translation error while FPT is active
    without error, that FPT will not react to the unrelated error state.
    
    Differential Revision: https://phabricator.services.mozilla.com/D216425

--- toolkit/components/translations/actors/TranslationsParent.sys.mjs
+++ toolkit/components/translations/actors/TranslationsParent.sys.mjs
@@ -812,16 +812,18 @@ export class TranslationsParent extends JSWindowActorParent {
   /**
    * Requests a new translations port.
    *
-   * @param {number} innerWindowId - The id of the current window.
    * @param {string} fromLanguage - The BCP-47 from-language tag.
    * @param {string} toLanguage - The BCP-47 to-language tag.
+   * @param {number} [innerWindowId] - The id of the current window.
+   *   This will associate this translations port with the TranslationsParent actor instance for the provided id,
+   *   which will mean that changes in the translation state will affect the state of the translations URL bar button etc.
    *
    * @returns {Promise<MessagePort | undefined>} The port for communication with the translation engine, or undefined on failure.
    */
   static async requestTranslationsPort(
-    innerWindowId,
     fromLanguage,
-    toLanguage
+    toLanguage,
+    innerWindowId
   ) {
     let translationsEngineParent;
     try {
@@ -3036,7 +3038,7 @@ class TranslationsLanguageState {
   }
 
   /**
-   * The last error that occured during translation.
+   * The last error that occurred during translation.
    */
   get error() {
     return this.#error;
