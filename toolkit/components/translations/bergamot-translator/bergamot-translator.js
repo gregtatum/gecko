720fc4ba96f1f0f997bb499356751e3bb45d5bbb
Erik Nordin <enordin@mozilla.com>
2024-10-28 19:44:18 -0500

    Bug 1917696 - Regenerate Bergamot WASM files from new fork r=#translations-reviewers!
    Now that the source and build logic have been moved upstream in
    https://github.com/mozilla/firefox-translations-training/pull/902
    we should regenerate the WASM artifcats using the new infrastructure
    to ensure that they are still equivalent and compatible in tree.
    
    This patch runs the scripts to build the new WASM artifacts and stores
    them in tree. Independent of this patch, we should publish a new minor
    version of the generated WASM binary to Remote Settings.
    
    There shouldn't be any major changes, but we should ensure that everything
    is building and running with our current infrastructure.
    
    Differential Revision: https://phabricator.services.mozilla.com/D226940

--- toolkit/components/translations/bergamot-translator/bergamot-translator.js
+++ toolkit/components/translations/bergamot-translator/bergamot-translator.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 function loadBergamot(Module) {
-  var BERGAMOT_VERSION_FULL = "v0.4.5+05a8778";
+  var BERGAMOT_VERSION_FULL = "v0.4.5+2ec5481";
   null;
 
   var Module = typeof Module != "undefined" ? Module : {};
